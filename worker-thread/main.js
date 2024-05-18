377e848dbf01534618c3477fd3d7ebdcc21c53e1
Alone <tranluong460@gmail.com>
2024-05-18 11:02:51 +0700

    up worker thread

--- worker-thread/main.js
+++ worker-thread/main.js
@@ -1,31 +1,22 @@
-const {
-  Worker,
-  workerData,
-  isMainThread,
-  parentPort,
-} = require("node:worker_threads");
+const http = require("node:http");
+const { Worker } = require("node:worker_threads");
 
-if (isMainThread) {
-  const worker = new Worker(__filename, { workerData: 20 });
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200);
+    res.end("Home page");
+  } else if (req.url === "/slow-page") {
+    const worker = new Worker("./worker-thread.js");
 
-  worker.on("message", (msg) => console.log(`Fibonacci: ${msg}`));
+    worker.on("message", (j) => {
+      res.writeHead(200);
+      res.end(`Slow Page ${j}`);
+    });
 
-  worker.on("error", (err) => console.error(err));
+    worker.on("error", (err) => {
+      console.log(err);
+    });
+  }
+});
 
-  worker.on("exit", (code) => console.error(code));
-} else {
-  const fibonacci = (n) => {
-    var i;
-    var fib = [];
-
-    fib[0] = 0;
-    fib[1] = 1;
-    for (i = 2; i <= n; i++) {
-      fib[i] = fib[i - 2] + fib[i - 1];
-    }
-
-    parentPort.postMessage(fib);
-  };
-
-  fibonacci(workerData);
-}
+server.listen(8080, () => console.log("Server is running on port 8080"));
