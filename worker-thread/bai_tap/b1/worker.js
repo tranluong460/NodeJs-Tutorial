dca093ff7e7d35070a25c394b4747b5daae48cf8
xuandtt-vitech <xuandtt@phanmemmkt.vn>
2025-12-02 14:41:35 +0700

    log to find step run

--- worker-thread/bai_tap/b1/worker.js
+++ worker-thread/bai_tap/b1/worker.js
@@ -1,5 +1,13 @@
 const {parentPort} = require("worker_threads");
+
+console.log("[worker] worker.js được load");
+
+// Lắng nghe message gửi từ main thread xuống worker thread
 parentPort.on("message",(n) => {
+    console.log("[worker] Nhận message từ main, n =", n);
     const result = n*n;
+    console.log("[worker] Tính xong n*n, result =", result);
+    // Gửi kết quả từ worker thread trở lại main thread
     parentPort.postMessage(result);
-})
+    console.log("[worker] Đã gửi result về cho main");
+});
